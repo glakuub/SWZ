99e66c5b7aa1dbedd7f50eafe15a8c8b351029a4
Jakub Gładysz <kuba997g@gmail.com>
2020-01-24 19:46:25 +0100

    Created resoureces dictionary with styles and data templates

---
+++ SWZ/Views/Dictionary.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml;
+
+namespace SWZ.Views
+{
+    public sealed partial class Dictionary: ResourceDictionary
+    {
+        public Dictionary()
+        {
+            InitializeComponent();
+        }
+    }
+}
